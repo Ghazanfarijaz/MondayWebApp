b572fddf02b66c60f2a742fa5e1b1aa10ad22dd8
Babar78 <muhammadbabarashraf8@gmail.com>
2025-06-18 00:29:01 +0500

    style: enhance loading and error states in ItemDetails component for better user experience

--- src/components/DashboardComponents/BoradDetails.jsx
+++ src/components/DashboardComponents/BoradDetails.jsx
@@ -11,19 +11,24 @@ const ItemDetails = () => {
   // Find the specific item by matching the ID
   const item = groupData?.find((item) => item.id === id);
 
-  if (!item) return <div className="p-[40px]">Item not found</div>;
+  if (!item)
+    return (
+      <div className="p-[40px] text-gray-500 dark:text-gray-400 blue:text-gray-400 flex items-center justify-center h-full bg-gray-200 dark:bg-light-black blue:bg-light-blue">
+        Item not found
+      </div>
+    );
 
   if (loading) {
     return (
-      <div className="p-[40px] bg-gray-200 flex items-center justify-center h-full">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
+      <div className="p-[40px] bg-gray-200 dark:bg-light-black blue:bg-light-blue flex items-center justify-center h-full">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900 dark:border-gray-100 blue:border-white"></div>
       </div>
     );
   }
 
   if (error) {
     return (
-      <div className="p-[40px] bg-gray-200 flex items-center justify-center h-full">
+      <div className="p-[40px] bg-gray-200 dark:bg-light-black blue:bg-light-blue flex items-center justify-center h-full">
         <p className="text-red-500">Error: {error}</p>
       </div>
     );
@@ -31,15 +36,15 @@ const ItemDetails = () => {
 
   if (usersLoading) {
     return (
-      <div className="p-[40px] bg-gray-200 flex items-center justify-center h-full">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
+      <div className="p-[40px] bg-gray-200 dark:bg-light-black blue:bg-light-blue flex items-center justify-center h-full">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900 dark:border-gray-100 blue:border-white"></div>
       </div>
     );
   }
 
   if (usersError) {
     return (
-      <div className="p-[40px] bg-gray-200 flex items-center justify-center h-full">
+      <div className="p-[40px] bg-gray-200 dark:bg-light-black blue:bg-light-blue flex items-center justify-center h-full">
         <p className="text-red-500">Error in user photothumb: {usersError}</p>
       </div>
     );
@@ -62,13 +67,25 @@ const ItemDetails = () => {
       </div>
 
       {/* Item title */}
-      <h1 className="text-3xl font-bold mb-8 text-black dark:text-white blue:text-white">
-        {item.name}
-      </h1>
+      <div className="flex items-center justify-between gap-8 mb-8">
+        <h1 className="text-3xl font-bold text-black dark:text-white blue:text-white break-all">
+          {item.name}
+        </h1>
+        <Link
+          to={`edit-details`}
+          className="px-4 py-2 bg-[#2A85FF] text-white rounded-lg hover:shadow-lg transition-colors duration-200"
+        >
+          Edit Item
+        </Link>
+      </div>
+
       <div className="bg-white dark:bg-black blue:bg-dark-blue p-6 rounded-lg shadow-sm">
         {item.column_values.map((columnValue, i) => {
           return columnValue.type === "status" ? (
-            <div className="grid grid-cols-[150px_1fr] gap-[32px] items-center mb-4">
+            <div
+              key={columnValue.id}
+              className="grid grid-cols-[150px_1fr] gap-[32px] items-center mb-4"
+            >
               <span className="text-gray-500 dark:text-[#6F767E] blue:text-gray-400">
                 {columnValue.column.title}
               </span>
@@ -80,7 +97,10 @@ const ItemDetails = () => {
               </span>
             </div>
           ) : columnValue.type === "people" ? (
-            <div className="grid grid-cols-[150px_1fr] gap-[32px] items-center mb-4">
+            <div
+              key={columnValue.id}
+              className="grid grid-cols-[150px_1fr] gap-[32px] items-center mb-4"
+            >
               <span className="text-gray-500 dark:text-[#6F767E] blue:text-gray-400">
                 {columnValue.column.title}
               </span>
@@ -101,7 +121,10 @@ const ItemDetails = () => {
               </div>
             </div>
           ) : (
-            <div className="grid grid-cols-[150px_1fr] gap-[32px] items-start mb-4">
+            <div
+              key={columnValue.id}
+              className="grid grid-cols-[150px_1fr] gap-[32px] items-start mb-4"
+            >
               <span className="text-gray-500 dark:text-[#6F767E] blue:text-gray-400">
                 {columnValue.column.title}
               </span>

d780992f7a3ea99ad50d1b1706829e356b538dbf
Babar78 <muhammadbabarashraf8@gmail.com>
2025-07-09 03:14:26 +0500

    feat: add profile picture upload and compression functionality

---
+++ src/features/settings/services/compressImageFile.js
@@ -0,0 +1,19 @@
+import imageCompression from "browser-image-compression";
+
+const compressImageFile = async (file) => {
+  // Image Compression Options
+  const options = {
+    maxSizeMB: 1,
+    maxWidthOrHeight: 1920,
+    useWebWorker: true,
+  };
+
+  try {
+    const compressedFile = await imageCompression(file, options);
+    return compressedFile;
+  } catch (error) {
+    return file;
+  }
+};
+
+export default compressImageFile;

ed9073d2cb0b06b58696d2884d23a0ab19815c49
Babar78 <muhammadbabarashraf8@gmail.com>
2025-07-15 22:30:14 +0500

    feat: added_skeletons_and_conditional_checks_in_signup_pages_based_on_signup_permission

--- src/pages/auth/signup/Signup.jsx
+++ src/pages/auth/signup/Signup.jsx
@@ -2,15 +2,16 @@ import { Lock, Mail, CircleUser } from "lucide-react";
 import { useForm } from "@mantine/form";
 import { TextInput, PasswordInput } from "@mantine/core";
 import { useMutation } from "@tanstack/react-query";
-import { toast } from "sonner";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../../assets/Logo.png";
 import GoogleIcon from "../../../assets/icons/GoogleIcon";
-import { authAPI } from "../../../api/auth";
+import { useSignUp } from "../../../contexts/SignUpContext";
+import SignupSkeleton from "../../../features/auth/components/SignupSkeleton";
+import { useEffect } from "react";
 
 const Signup = () => {
   const navigate = useNavigate();
+  const { isFetchingSignUpPermission, signUpMethod } = useSignUp();
 
   const SignupForm = useForm({
     initialValues: {
@@ -30,97 +31,102 @@ const Signup = () => {
     },
   });
 
-  const loginUser = useMutation({
-    mutationFn: () =>
-      authAPI.login({
-        email: SignupForm.values.email,
-        password: SignupForm.values.password,
-        slug: window.location.hostname.split(".")[0],
-        // slug: "proto-it-consultants",
-      }),
-    onSuccess: (data) => {
-      localStorage.setItem("userData", JSON.stringify(data.user));
-      // Redirect to main page on success
-      navigate("/", { replace: true });
-    },
-    onError: (error) => {
-      toast.error("Login failed", {
-        description: error.message || "Please try again.",
-      });
-      console.error("Login error:", error);
-    },
-  });
+  const signup = useMutation({});
+
+  useEffect(() => {
+    if (
+      !isFetchingSignUpPermission &&
+      (signUpMethod !== "signup-with-admin-approval" ||
+        signUpMethod === "signup-without-admin-approval")
+    ) {
+      console.error("Sign up not allowed");
+      navigate("/auth/login", { replace: true });
+    }
+  }, [isFetchingSignUpPermission, signUpMethod, navigate]);
 
   return (
     <main className="font-medium w-[352px]">
       <img src={Logo} alt="Logo" className="object-contain w-[48px] h-[48px]" />
       <h1 className="mt-8 text-4xl font-semibold text-slate-900">Sign up</h1>
-      <form
-        // onSubmit={SignupForm.onSubmit(loginUser.mutate)}
-        onSubmit={(e) => {
-          e.preventDefault();
-          navigate("/signup/otp");
-        }}
-        className="mt-8 flex flex-col gap-4"
-      >
-        <TextInput
-          // label="Your Name"
-          variant="filled"
-          placeholder="Your Name"
-          {...SignupForm.getInputProps("name")}
-          leftSectionPointerEvents="none"
-          leftSection={<CircleUser size={20} className="text-gray-500" />}
-          classNames={{
-            input: "!h-[48px] !rounded-lg !ps-10",
-          }}
-        />
-        <TextInput
-          // label="Email"
-          variant="filled"
-          placeholder="Enter Your Email"
-          {...SignupForm.getInputProps("email")}
-          leftSectionPointerEvents="none"
-          leftSection={<Mail size={20} className="text-gray-500" />}
-          classNames={{
-            input: "!h-[48px] !rounded-lg !ps-10",
-          }}
-        />
-        <PasswordInput
-          // label="Password"
-          variant="filled"
-          placeholder="Enter Your Password"
-          {...SignupForm.getInputProps("password")}
-          leftSectionPointerEvents="none"
-          leftSection={<Lock size={20} className="text-gray-500" />}
-          classNames={{
-            input: "!h-[48px] !rounded-lg !ps-10",
-          }}
-        />
 
-        <button
-          type="submit"
-          className={`mt-4 text-white py-3 rounded-lg font-medium transition-colors bg-blue-600 hover:bg-blue-700 cursor-pointer disabled:bg-blue-400 disabled:cursor-not-allowed`}
-          disabled={loginUser.isPending}
-        >
-          {loginUser.isPending ? "Signing up..." : "Sign up"}
-        </button>
-      </form>
-      {/* Google Signin */}
-      <div className="flex items-center mt-8 mb-5">
-        <hr className="flex-1 border-b-1 border-[#D6D6D6]" />
-        <p className="font-bold text-[20px]/[150%] px-4 text-[#5E5E5E]">OR</p>
-        <hr className="flex-1 border-b-1 border-[#D6D6D6]" />
-      </div>
-      <button className="flex items-center gap-2 justify-center w-full bg-[#FCFCFC] border-2 border-[#EFEFEF] rounded-lg py-3 text-[#1A1D1F] font-bold  hover:bg-gray-100 text-[15px]/[24px] mb-8">
-        <GoogleIcon className="size-6" />
-        Google
-      </button>
-      <p className="text-center text-[#808494] text-[14px]/[140%] font-medium">
-        Already have an account?{" "}
-        <Link className="font-bold text-[#000929]" to="/auth/login">
-          Sign In
-        </Link>
-      </p>
+      {isFetchingSignUpPermission ? (
+        <SignupSkeleton />
+      ) : (
+        <>
+          <form
+            onSubmit={SignupForm.onSubmit(signup.mutate)}
+            className="mt-8 flex flex-col gap-8"
+          >
+            <div className="flex flex-col gap-4">
+              <TextInput
+                // label="Your Name"
+                variant="filled"
+                placeholder="Your Name"
+                {...SignupForm.getInputProps("name")}
+                leftSectionPointerEvents="none"
+                leftSection={<CircleUser size={20} className="text-gray-500" />}
+                classNames={{
+                  input: "!h-[48px] !rounded-lg !ps-10",
+                }}
+              />
+              <TextInput
+                // label="Email"
+                variant="filled"
+                placeholder="Enter Your Email"
+                {...SignupForm.getInputProps("email")}
+                leftSectionPointerEvents="none"
+                leftSection={<Mail size={20} className="text-gray-500" />}
+                classNames={{
+                  input: "!h-[48px] !rounded-lg !ps-10",
+                }}
+              />
+              <PasswordInput
+                // label="Password"
+                variant="filled"
+                placeholder="Enter Your Password"
+                {...SignupForm.getInputProps("password")}
+                leftSectionPointerEvents="none"
+                leftSection={<Lock size={20} className="text-gray-500" />}
+                classNames={{
+                  input: "!h-[48px] !rounded-lg !ps-10",
+                }}
+              />
+            </div>
+
+            <button
+              type="submit"
+              className={`text-white py-3 rounded-lg font-medium transition-colors bg-blue-600 hover:bg-blue-700 cursor-pointer disabled:bg-blue-400 disabled:cursor-not-allowed`}
+              disabled={signup.isPending}
+            >
+              {signup.isPending ? "Signing up..." : "Sign up"}
+            </button>
+          </form>
+
+          <div className="flex flex-col gap-8">
+            {/* Google Signin */}
+            <div className="w-full mt-5 flex flex-col gap-5">
+              <div className="flex items-center">
+                <hr className="flex-1 border-b-1 border-[#D6D6D6]" />
+                <p className="font-bold text-[20px]/[150%] px-4 text-[#5E5E5E]">
+                  OR
+                </p>
+                <hr className="flex-1 border-b-1 border-[#D6D6D6]" />
+              </div>
+              <button className="flex items-center gap-2 justify-center w-full bg-[#FCFCFC] border-2 border-[#EFEFEF] rounded-lg py-3 text-[#1A1D1F] font-bold  hover:bg-gray-100 text-[15px]">
+                <GoogleIcon className="size-6" />
+                Google
+              </button>
+            </div>
+            {/* Already have an account */}
+            <p className="text-center text-[#808494] text-[14px]/[140%] font-medium">
+              Already have an account?{" "}
+              <Link className="font-bold text-[#000929]" to="/auth/login">
+                Sign In
+              </Link>
+            </p>
+          </div>
+        </>
+      )}
     </main>
   );
 };

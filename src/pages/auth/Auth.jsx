bdf1183f16d5d80462f35dd68e396e364d593081
Babar78 <muhammadbabarashraf8@gmail.com>
2025-07-19 02:26:43 +0500

    refactor: updated_bg_color_of_auth_Layout

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -3,12 +3,12 @@ import SIGN_IN_IMAGE from "../../assets/signin-image.svg";
 
 const Auth = () => {
   return (
-    <div className="w-screen h-screen grid lg:grid-cols-2">
-      <div className="lg:p-[0px_48px] p-[48px_24px] flex items-center justify-center bg-gray-50">
+    <div className="w-screen h-screen grid lg:grid-cols-2 bg-white">
+      <div className="lg:p-[0px_48px] p-[48px_24px] flex items-center justify-center">
         <Outlet />
       </div>
       {/* Image Section - With right padding */}
-      <div className="hidden lg:flex items-center justify-center p-12 bg-gray-50 h-full max-h-screen">
+      <div className="hidden lg:flex items-center justify-center p-12 h-full max-h-screen">
         {/* <SigninImage /> */}
         <img src={SIGN_IN_IMAGE} alt="portal-snapshot" className="h-full" />
       </div>

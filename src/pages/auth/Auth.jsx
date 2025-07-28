da99ed0d265606931b0ebf7eecd499d72b7d0bf6
Babar78 <muhammadbabarashraf8@gmail.com>
2025-07-28 21:55:11 +0500

    refactor: updated_auth_page_layout

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { useSignUp } from "../../contexts/SignUpContext";
-import AppLogo from "../../assets/logo-icon.jpeg";
+import AppLogo from "../../assets/mainlogo.jpg";
 
 const Auth = () => {
   const { userLogo, userDescription } = useSignUp();
@@ -9,36 +9,34 @@ const Auth = () => {
     <div className="w-screen h-screen grid lg:grid-cols-2 bg-white">
       <div className="lg:p-[0px_48px] p-[48px_24px] flex items-center justify-center">
         <div className="w-[352px]">
-          <div className="flex flex-col gap-2">
-            <img
-              src={AppLogo}
-              alt="app-logo"
-              className="object-contain w-[48px] h-[48px] rounded-lg"
-            />
-            <div>
-              <h3 className="font-bold text-xl text-[#f04967] font-serif">
-                Lucie{" "}
-                <span className="inline-block scale-y-110 origin-bottom">
-                  UP
-                </span>
-              </h3>
-              <p className="text-[#2c5da0] text-[10px] font-serif">
-                External <span className="text-[#f04967]">U</span>sers{" "}
-                <span className="text-[#f04967]">P</span>ortal
-              </p>
-            </div>
-          </div>
+          <img
+            src={AppLogo}
+            alt="app-logo"
+            className="object-contain w-[95px] h-auto"
+          />
           <Outlet />
         </div>
       </div>
       {/* Image Section - With right padding */}
       <div className="hidden lg:flex items-center justify-center p-12 h-full max-h-screen">
         {/* <SigninImage /> */}
-        <div className="h-fit max-h-[320.275px] w-full bg-gray-200/70 rounded-3xl flex flex-col gap-4 relative p-8">
-          <img src={userLogo} alt="user-logo" className="w-16 rounded-lg" />
-          <p className="text-lg text-gray-500 line-clamp-4">
-            {userDescription}
-          </p>
+        <div
+          className={`h-full ${
+            window.location.pathname.split("/")[2] === "signup"
+              ? "max-h-[635.53px]"
+              : "max-h-[609.53px]"
+          } w-full bg-gray-200/70 rounded-3xl flex flex-col justify-between gap-8 p-4`}
+        >
+          <div className="flex flex-col gap-4">
+            <img
+              src={userLogo}
+              alt="user-logo"
+              className="w-[95px] h-auto rounded-lg object-contain"
+            />
+            <p className="text-lg text-gray-500 line-clamp-4">
+              {userDescription}
+            </p>
+          </div>
           <div className="mt-4 flex items-center justify-end w-full">
             <p className="text-[14px] p-[8px_16px] rounded-full bg-gray-300 border border-gray-300">
               A Eurotas Solution

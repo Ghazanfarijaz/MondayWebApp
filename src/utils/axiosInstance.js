f4cb6f0b73146f6ee95c90ad7993c7a07033fc59
Babar78 <muhammadbabarashraf8@gmail.com>
2025-07-30 03:06:03 +0500

    refactor: udpated_live_url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://ef014-service-23360785-a4c1a002.us.monday.app";
+const BASE_URL = "https://c138b-service-23360785-a4c1a002.us.monday.app";
 // const BASE_URL = "http://localhost:8080";
 
 export const axiosInstance = axios.create({

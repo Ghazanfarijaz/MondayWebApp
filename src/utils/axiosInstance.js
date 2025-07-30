58c0201e9c65ea6e4a71d345559c329750f4c900
Babar78 <muhammadbabarashraf8@gmail.com>
2025-07-31 02:56:31 +0500

    feat: updated_live_url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "https://c138b-service-23360785-a4c1a002.us.monday.app";
+// const BASE_URL_BEFORE_SANITIZATION = "https://c138b-service-23360785-a4c1a002.us.monday.app";
+const BASE_URL = "https://cb471-service-23360785-a4c1a002.us.monday.app";
 // const BASE_URL = "http://localhost:8080";
 
 export const axiosInstance = axios.create({

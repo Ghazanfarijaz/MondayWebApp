091361ab9730eadd2b0022bd7055ab9572255ac3
Babar78 <muhammadbabarashraf8@gmail.com>
2025-09-17 19:31:25 +0500

    fix: update BASE_URL to correct endpoint

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-// const BASE_URL_BEFORE_SANITIZATION = "https://c138b-service-23360785-a4c1a002.us.monday.app";
-
-const BASE_URL = "https://e2aff-service-23360785-a4c1a002.us.monday.app";
+const BASE_URL = "https://bc008-service-23360785-a4c1a002.us.monday.app";
 // const BASE_URL = "http://localhost:8080";
 
 export const axiosInstance = axios.create({

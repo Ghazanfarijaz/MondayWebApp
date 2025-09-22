4a45a147a6186dd10497c78a136d4d0b2706b347
babar78 <muhammadbabarashraf8@gmail.com>
2025-09-22 09:55:46 -0700

    fix: adjust grid layout in BoardGroup component for improved responsiveness

--- src/features/dashboard/components/BoardGroup.jsx
+++ src/features/dashboard/components/BoardGroup.jsx
@@ -284,7 +284,7 @@ const BoardGroup = ({
                           {items[0]?.group?.title || "Group"}
                         </h2>
                       </div>
-                      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-4">
                         {items?.map((item) => (
                           <CardItem key={item.id} item={item} />
                         ))}
